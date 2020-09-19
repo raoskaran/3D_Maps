951db29b0422d21621a1fb9ccf1710478487c4b5
shadow-guardiann <karan.kr.1398@gmail.com>
2020-09-19 12:41:32 +0530

    confirming

--- visualization_techniques.py
+++ visualization_techniques.py
@@ -55,8 +55,8 @@ def display_inlier_outlier(cloud, ind):
 
 #----------------------------------------------Preprocessing----------------------------------------------
 
-pcd = o3d.io.read_point_cloud("data/scenes/continous_final.xyz")
-cloud = PyntCloud.from_file("data/scenes/nochair.xyz", sep=" ")
+pcd = o3d.io.read_point_cloud("data/scenes/helios.xyz")
+cloud = PyntCloud.from_file("data/scenes/helios.xyz", sep=" ")
 times = []
 algorithms = ['Ball Pivoting','Tetrahedral','Alpha','Delaunay','Poisson','MC(Pymcubes)','MC(Skimage)']
 
